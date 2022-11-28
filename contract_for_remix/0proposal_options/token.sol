ff1c5074abeb6d0c2f347345aad1b220c5a8adf7
BahaMert <bersoy@sabanciuniv.edu>
2022-11-28 20:37:33 +0300

    pair coded, proposaln info for fornt end implemented

---
+++ contract_for_remix/0proposal_options/token.sol
@@ -0,0 +1,78 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract SUToken is ERC20, Ownable {
+
+    //mapping (uint => mapping(address => bool)) public proposalid_to_address_cantusetoken;
+    uint256 public proposal_num;
+    address public myDAO;
+    mapping (address => mapping(uint256 => bool)) public transferLock;
+    
+
+
+    constructor(string memory name, string memory symbol, address owner) ERC20(name, symbol) {
+        _mint(owner, 1000 * 10 ** decimals());
+        proposal_num = 0;
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        //bu function cagirilan, _mint kendisinde iste boylece degisitrebliyorum anladin heralde
+        _mint(to, amount);
+    }
+
+    function assignDAO(address in_dao) public onlyOwner{
+        myDAO = in_dao;
+    }
+
+    //between people
+    function transfer(address to, uint256 amount) public virtual override returns (bool) {
+        address owner = _msgSender();
+        for (uint i = 0; i < proposal_num; i ++ ){
+            if(transferLock[owner][i] == true){
+                //address_proposals_cant_vote_on[to][i] = address_proposals_cant_vote_on[owner][i];
+                require(false , "have active voting");
+            }
+        }      
+        _transfer(owner, to, amount);
+        return true;
+    }
+
+    //when DAO does it
+    function transferDAO(address to, uint256 amount) public returns (bool) {
+        address owner = _msgSender();
+        require(owner == myDAO);
+        //when dao sends it this is not the case
+        _transfer(owner, to, amount);
+        return true;
+    }
+
+    function update_proposal_num(uint proposals) public {
+        address owner = msg.sender;
+        require(owner == myDAO);
+        proposal_num = proposals;
+    }
+
+    function update_active_voter_lock_on(uint proposal, address sender) public {
+        address owner = msg.sender;
+        require(owner == myDAO);
+        transferLock[sender][proposal] = true;
+    }    
+
+    function update_active_voter_lock_off(uint proposal, address sender) public {
+        address owner = msg.sender;
+        require(owner == myDAO);
+        transferLock[sender][proposal] = false;
+    }    
+
+    //soyle oncelikle factory de tokenime dao adresim giriliyor, sonra ben yk olarak birine token gonderirsem bir sorun yok, ama normal 
+    //bir sekilde transfer ile gonderirsem butun oyladigim proposallarda digerininkileri aliyorum, yani onun oy verdiklerine oy veremiyorum ve bu.
+    //sirf true olanlarda gecerli yani bos bir adresten transferlede duzeltilemiyor, sonrasinda oy veriyorum oy verirken bu kisininki
+    //oyle tutulmus mu diye  checkleniyor. sonrasinda direk ona gore geri donus aliyorum.
+
+
+    //function isContract(address addr) view private returns (bool isContract) {    return addr.code.length > 0; }
+
+}
